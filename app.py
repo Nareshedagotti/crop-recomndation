803a668975bc0885dc994945062c8922a11a468d
Naresh Edagotti <naresh21375019@gmail.com>
2023-12-30 16:30:05 +0530

    template for crop recommendation app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import pickle
 
@@ -8,15 +8,28 @@ app = Flask(__name__)
 naive_bayes_model = pickle.load(open('naive_bayes_model.pkl', 'rb'))
 label_encoder = pickle.load(open('label_encoder.pkl', 'rb'))
 
+# Route to render the HTML form
+@app.route('/')
+def index():
+    return render_template('template.html')
+
 # Endpoint to receive input data and make predictions using Naive Bayes
 @app.route('/predict_nb', methods=['POST'])
 def predict_naive_bayes():
     try:
-        # Get input data from the request
-        input_data = request.get_json()
+        # Get input data from the form
+        input_data = {
+            'N': float(request.form['N']),
+            'P': float(request.form['P']),
+            'K': float(request.form['K']),
+            'temperature': float(request.form['temperature']),
+            'humidity': float(request.form['humidity']),
+            'ph': float(request.form['ph']),
+            'rainfall': float(request.form['rainfall']),
+        }
 
         # Convert input data to a DataFrame
-        input_df = pd.DataFrame(input_data)
+        input_df = pd.DataFrame([input_data])
 
         # Make predictions using the Naive Bayes model
         nb_prediction = naive_bayes_model.predict(input_df)
@@ -32,3 +45,4 @@ def predict_naive_bayes():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
